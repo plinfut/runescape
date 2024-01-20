0cca714a490287d80f6cbde81832d5c19e3e788b
Plinfut <plinfut@gmail.com>
2024-01-20 03:06:50 +0100

    Bugfix: set f2p prio

--- app/Helpers/Functions/targets.php
+++ app/Helpers/Functions/targets.php
@@ -9,9 +9,9 @@ function createTarget(string $description, bool $members_only, int $priority, st
         'members_only_own' => $members_only,
         'members_only_inh' => $members_only,
         'p2p_prio_own' => $priority,
-        'f2p_prio_own' => $members_only ? $priority : MAX_PRIO,
+        'f2p_prio_own' => $members_only ? MAX_PRIO : $priority,
         'p2p_prio_inh' => $priority,
-        'f2p_prio_inh' => $members_only ? $priority : MAX_PRIO,
+        'f2p_prio_inh' => $members_only ? MAX_PRIO : $priority,
         'p2p_usefulness' => MAX_PRIO - $priority,
         'f2p_usefulness' => $members_only ? 0 : MAX_PRIO - $priority,
     ]);
